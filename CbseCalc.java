2aeb997335237ade20c5dbde3fd69683f8919edf
Riddhi S <114600109+xriddhi@users.noreply.github.com>
2022-09-29 23:18:05 +0530

    Update and rename Java.java to CbseCalc.java

--- CbseCalc.java
+++ CbseCalc.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Java {
+public class CbseCalc {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Subject One Marks:");
@@ -7,7 +7,12 @@ public class Java {
         System.out.println("Enter Subject Two Marks:");
         Float subTwo = sc.nextFloat();
         Float Sum = subOne + subTwo;
-        Float Perc = Sum/200*100;
+         System.out.println("Enter Subject One Total Marks:");
+        Float totalMarksOne = sc.nextFloat();
+        System.out.println("Enter Subject Two Total Marks:");
+        Float totalMarksTwo = sc.nextFloat();
+        Float SumTotal = totalMarksOne+totalMarksTwo;
+        Float Perc = Sum/SumTotal*100;
         System.out.println(Perc);
 
 
