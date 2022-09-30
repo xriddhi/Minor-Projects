b830dffe65127bea1fbb35984440bf0190a8f616
Riddhi S <114600109+xriddhi@users.noreply.github.com>
2022-09-30 20:09:15 +0530

    Add files via upload

---
+++ MiniProjects.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class MiniProjects {
+    public static void main(String[] args) {
+        //1.
+        //Percentage Calculator:
+
+        //Scanner scanner = new Scanner(System.in)
+           // System.out.println("Enter Subject One Marks:");
+           // Float subOne = scanner.nextFloat();
+            //System.out.println("Enter Subject Two Marks:");
+           // Float subTwo = scanner.nextFloat();
+           // Float Sum = subOne + subTwo;
+            //Float Perc = Sum/200*100;
+           // System.out.println(Perc);
+
+
+
+
+
+
+
+
+
+        //2.
+        //Guess the Number:
+
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter A Number:");
+        //int numUser = sc.nextInt();
+        //int number = 15;
+
+       // if(number>numUser){
+         //   System.out.println("Oops,The Required Number Is Greater Than Your Number!");
+        //}else if(numUser>number){
+         //   System.out.println("Oops,The Required Number Is Less Than Your Number!");
+       // }else{
+           // System.out.println("Correct!");
+
+
+           
+        }
+
+    }
