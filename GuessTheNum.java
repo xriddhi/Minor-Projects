7d87bb3f580644583afe6ec248af0669570b7462
Riddhi S <114600109+xriddhi@users.noreply.github.com>
2022-09-30 20:10:08 +0530

    Update and rename MiniProjects.java to GuessTheNum.java

--- GuessTheNum.java
+++ GuessTheNum.java
@@ -1,27 +1,6 @@
 import java.util.Scanner;
-public class MiniProjects {
+public class GuessTheNum {
     public static void main(String[] args) {
-        //1.
-        //Percentage Calculator:
-
-        //Scanner scanner = new Scanner(System.in)
-           // System.out.println("Enter Subject One Marks:");
-           // Float subOne = scanner.nextFloat();
-            //System.out.println("Enter Subject Two Marks:");
-           // Float subTwo = scanner.nextFloat();
-           // Float Sum = subOne + subTwo;
-            //Float Perc = Sum/200*100;
-           // System.out.println(Perc);
-
-
-
-
-
-
-
-
-
-        //2.
         //Guess the Number:
 
         //Scanner sc = new Scanner(System.in);
